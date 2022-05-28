2f7b7775d88b6ffa9c3240ee0c9085ad2bea982e
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-05-28 11:01:44 -0400

    improve subprocess

--- vidsegment.py
+++ vidsegment.py
@@ -28,11 +28,14 @@ def split_video(dest_path: str, filename: str, segments: Iterable[Segment], over
         dest_filename = segment.filename if segment.filename is not None else segment.title + extension
         dest_filepath = os.path.join(dest_path, dest_filename)
 
-        arguments = ['ffmpeg', '-i', filename, '-c', 'copy', '-ss', str(segment.start), '-to', str(segment.end)]
-        if overwrite:
-            arguments.append('-y')
-        else:
-            arguments.append('-n')
+        arguments = [
+            'ffmpeg',
+            '-i', filename,
+            '-c', 'copy',
+            '-ss', str(segment.start),
+            '-to', str(segment.end),
+            '-y' if overwrite else '-n'
+        ]
         if segment.origin is not None:
             arguments.extend(['-metadata', f'comment={segment.origin}'])
 
@@ -40,9 +43,11 @@ def split_video(dest_path: str, filename: str, segments: Iterable[Segment], over
 
         os.makedirs(os.path.dirname(dest_filepath), exist_ok=True)
 
-        result = subprocess.run(arguments)
-        if result.returncode != 0:
-            raise RuntimeError()
+        with subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
+            _, stderr = process.communicate()
+            if process.returncode != 0:
+                print(stderr.decode('utf-8'), file=sys.stderr)
+                raise RuntimeError()
 
 def load_segments(filename: str) -> List[Segment]:
     with open(filename, 'r') as file:
